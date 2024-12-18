d6e64e588b04b28ffad52cafab9549cc2d897f53
Gabriele-Viola <gabrieleviola50@gmail.com>
2024-12-18 12:49:09 +0100

    add details at single person

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -10,6 +10,12 @@ export default function Contacts() {
     const { id } = useParams()
     const [travel, setTravel] = useState(travels[id])
     console.log(travel);
+
+    function handletoggle(e) {
+
+        document.getElementById(`${e.target.id + 1}`).classList.toggle('d-none')
+
+    }
     return (
         <>
             <div className="container text-center">
@@ -18,10 +24,31 @@ export default function Contacts() {
 
                 <h1 className="mb-4">{travel.destinazione}</h1>
                 {filteredParticipants.map((single, index) =>
-                    <div className="card w-50 m-auto mb-3" key={index}>
-                        <div className="card-body">
-                            <span>{single.nome} </span>
-                            <span>{single.cognome}</span>
+                    <div className="card col-9 col-md-6 col-lg-4 m-auto mb-3" key={index}>
+                        <div className="card-body" id={index}>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <div>
+                                    <span>{single.nome} </span>
+                                    <span>{single.cognome}</span>
+
+                                </div>
+                                <i onClick={handletoggle} id={single.codice_fiscale} className="bi bi-arrow-down-short fs-2"></i>
+
+                            </div>
+
+
+                            <div className={`d-none text-start`} id={single.codice_fiscale + 1}>
+                                <div>
+                                    email: {single.email}
+                                </div>
+                                <div>
+                                    codice fiscale: {single.codice_fiscale}
+                                </div>
+                                <div>
+                                    numero di telefono: {single.numero_telefono}
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                 )}
